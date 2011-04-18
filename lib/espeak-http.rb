2f8fd796c49054834ac634d860fa8c133ae3fa5a
ujjawal <ujjawal@wordify.me>
2011-04-18 23:40:12 +0530

    using local espeak-ruby for more espeak config flexibility

--- lib/espeak-http.rb
+++ lib/espeak-http.rb
@@ -7,8 +7,10 @@ include ESpeak
 
 get '/tts' do
   filename = "tmp/#{Digest::SHA1.hexdigest(params.to_s)}.mp3"
+  params['voice'] = '+m2'
   params['speed'] = 140
-  params['pitch'] = 10
+  params['pitch'] = 5
+  params['amplitude'] = 50
   espeak(filename, params) # unless filename exists
   [200, {'Content-type' => 'audio/mpeg'}, File.read(filename)]
 end

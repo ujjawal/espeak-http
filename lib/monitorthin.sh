7f30e6819c67d25cf66e22dea603335f6862a9fd
ujjawal <ujjawal@wordify.me>
2011-04-23 16:51:59 +0530

    Adding a thin monitoring script. Should be added as a cron to keep thin running

---
+++ lib/monitorthin.sh
@@ -0,0 +1,18 @@
+ifthin=`ps -eo pid,comm | grep thin | grep -v grep | awk '{print $1}'`
+j=0;
+if [ -z "$ifthin" ]
+then
+    thin -s 4 start -O -p 5000
+else
+    for i in $ifthin
+    do
+        j="$(( $j + 1 ))";
+    done    
+    if [ $j -lt 4 ]
+    then
+        thin -s 4 restart -O -p 5000
+    fi
+    echo 'thin'
+fi
+
+

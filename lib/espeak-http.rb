f892fc322344106f99d6b4bd788f6cf8bc0c1ca2
ujjawal <ujjawal@wordify.me>
2011-04-23 18:50:58 +0530

    changes to send the audio in response without writing to a file

--- lib/espeak-http.rb
+++ lib/espeak-http.rb
@@ -1,16 +1,8 @@
-%w(rubygems 
-   sinatra 
-   espeak-ruby
-   digest/sha1).each { |l| require l }
-
-include ESpeak
-
+require 'sinatra'
 get '/tts' do
-  filename = "tmp/#{Digest::SHA1.hexdigest(params.to_s)}.mp3"
   params['voice'] = '+m2'
   params['speed'] = 140
   params['pitch'] = 5
   params['amplitude'] = 50
-  espeak(filename, params) # unless filename exists
-  [200, {'Content-type' => 'audio/mpeg'}, File.read(filename)]
+  [200, {'Content-type' => 'audio/mpeg'}, `espeak #{params[:text]} --stdout -v#{params[:voice]} -p#{params[:pitch]} -s#{params[:speed]} -a#{params[:amplitude]}  | lame -V8 - `]
 end
